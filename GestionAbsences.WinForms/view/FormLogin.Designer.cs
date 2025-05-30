3ad9ea0dd32a6575a8fba3976d733763e9a36ec4
fayssal elhouari <fayssalelhouaripro@gmail.com>
2025-05-30 17:25:54 +0200

    Phase 1 : Création d'application et le visuel des interfaces
    Application Windows Forms (C#/.NET Framework) pour saisir, modifier et consulter les absences du personnel.

---
+++ GestionAbsences.WinForms/view/FormLogin.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GestionAbsences.WinForms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mot de passe";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(15, 29);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(395, 20);
+            this.txtLogin.TabIndex = 2;
+            this.txtLogin.TextChanged += new System.EventHandler(this.txtLogin_TextChanged);
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(15, 79);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(395, 20);
+            this.textPassword.TabIndex = 3;
+            this.textPassword.TextChanged += new System.EventHandler(this.textPassword_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(46, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Se connecter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 157);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Connexion  Gestion des Absences";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+### Interface et fonctionnalités
+Voici à quoi doit ressembler la fenêtre de l'application :<br>
+![1-Ecran](https://github.com/user-attachments/assets/b882223b-e430-4485-a99f-37f9bb144aa5)
+<br>
+
+# GestionAbsences.WinForms
+
+Application Windows Forms en C# (.NET Framework 4.7.2) pour la gestion des absences du personnel.  
+Ce projet suit une architecture MVC et propose :
+
+- Un formulaire de connexion (FormLogin)  
+- Un écran de gestion du personnel (FormPersonnel)  
+- Un écran de gestion des absences (FormAbsence)  
+
+## Prérequis
+
+- Visual Studio 2022  
+- .NET Framework 4.7.2 ou supérieur  
+
+## Installation
+
+```bash
+git clone https://github.com/votre-compte/GestionAbsences.WinForms.git
+cd GestionAbsences.WinForms
